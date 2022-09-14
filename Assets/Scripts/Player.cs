cf1126dd2f86a70e1646a91a13139b6c6805ada2
kingmozzi <kingmozzi@gmail.com>
2022-09-14 17:49:08 +0900

    2022-09-14

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    float hAxis;
+    float vAxis;
+    bool wDown;
+    bool jDown;
+
+    bool isJump;
+    bool isDodge;
+
+    Vector3 moveVec;
+    Vector3 dodgeVec;
+
+    Rigidbody rigid;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+        Dodge();
+    }
+
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
+        jDown = Input.GetButtonDown("Jump");
+    }
+
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+        if(isDodge)
+        {
+            moveVec = dodgeVec;
+        }
+        transform.position += moveVec * speed * (wDown?0.3f:1f) * Time.deltaTime;
+        //rigid.MovePosition(moveVec * speed * (wDown?0.3f:1f) * Time.deltaTime);
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("isWalk", wDown);
+    }
+
+    void Turn()
+    {
+        transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if(jDown && moveVec == Vector3.zero && !isJump && !isDodge)
+        {
+            rigid.AddForce(Vector3.up * 9, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+
+    void Dodge()
+    {
+        if(jDown && moveVec != Vector3.zero && !isDodge && !isJump)
+        {
+            dodgeVec = moveVec;
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.4f);
+        }
+    }
+
+    void DodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
+    }
+
+    void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump =false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # quarterView
 골드메탈  - 3D 쿼터뷰 액션게임
+https://www.youtube.com/c/GoldMetal
+https://www.goldmetal.co.kr/
